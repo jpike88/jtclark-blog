593375cdf5f569fe90f3caf9bfab2c981c0aae01
john@cracklecat.com <john@cracklecat.com@51583b24-cc36-0e9a-4bc2-685ace4ad1da>
2010-09-14 14:03:20 +0000

    Added Zend Framework Quickstart Project with Simple Ajax Tutorial code

---
+++ trunk/Zend Framework/Projects/quickstart-with-simple-ajax/application/controllers/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+{
+    $ajaxContext = $this->_helper->getHelper('AjaxContext');
+    $ajaxContext->addActionContext('list', 'html')
+                ->addActionContext('modify', 'html')
+                ->initContext();
+}
+ 
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+public function listAction() {
+    // pretend this is a sophisticated database query
+    $data = array('red','green','blue','yellow');
+    $this->view->data = $data;
+}
+}
+
